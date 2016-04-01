e64a56191aac0b63f107d18a95ba4e560f89b969
matthesrieke <m.rieke@52north.org>
2016-04-01 15:55:35 +0200

    adjust enforcer config

--- src/main/java/org/n52/aviation/aviationfx/FXMLController.java
+++ src/main/java/org/n52/aviation/aviationfx/FXMLController.java
@@ -8,18 +8,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 public class FXMLController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 }
--- src/main/java/org/n52/aviation/aviationfx/MainApp.java
+++ src/main/java/org/n52/aviation/aviationfx/MainApp.java
@@ -13,10 +13,10 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
+
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
-        
+
         stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
         stage.show();
